1bc9c2add0d82eb389aa1b39afe6aa954dd283df
Han Shao <hshao@oberlin.edu>
2019-05-06 02:15:08 -0400

    edit pool size

--- cnn/model_v2.py
+++ cnn/model_v2.py
@@ -68,7 +68,7 @@ def main(unused_args):
             activation=tf.nn.relu)
 
         # Pooling Layer #1
-        pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+        pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[3, 3], strides=2)
 
         # Convolutional Layer #2 and Pooling Layer #2
         conv2 = tf.layers.conv2d(
@@ -77,7 +77,7 @@ def main(unused_args):
             kernel_size=[3, 3],
             padding="valid",
             activation=tf.nn.relu)
-        pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+        pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[3, 3], strides=2)
 
         # Convolutional Layer #3 and Pooling Layer #3
         conv3 = tf.layers.conv2d(
@@ -90,14 +90,14 @@ def main(unused_args):
         conv4 = tf.layers.conv2d(
             inputs=conv3,
             filters=384,
-            kernel_size=[3,3],
+            kernel_size=[3, 3],
             padding="valid",
             activation=tf.nn.relu)
 
         conv5 = tf.layers.conv2d(
             inputs=conv4,
             filters=256,
-            kernel_size=[3,3],
+            kernel_size=[3, 3],
             padding="valid",
             activation=tf.nn.relu)
 
